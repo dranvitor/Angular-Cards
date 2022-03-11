1d0e224956c0b41b92c29521dc6529e6ac956d00
dranvitor <dranvitor@gmail.com>
2022-03-11 14:29:04 -0300

    atualizaçao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  
-  primeiroStorage:string ='500 GB Storage'
-  primeiroUser:string = '2 Users Allowed'
-  primeiroSendUp:string = 'Send up to 3 GB'
 
-  segundoStorage:string = '1 TB Storage'
-  segundoUser:string = '5 Users Allowed'
-  segundoSendUp:string = 'Send up to 10 GB'
-
-  terceiroStorage:string = '2 TB Storage'
-  terceiroUser:string = '10 Users Allowed'
-  terceiroSendUp:string = 'Send up to 20 GB'
+  // Os retangulos são caracteres invisíveis, eles foram utilizados pois o mat-divider não estava permitindo nem alterar a cor da linha, nem alterar o tamanho delas. Portanto eu retirei o mat-divider e o substituí por borders, fazendo o tamanho das linhas ser aumentado junto ao texto.
   
+  primeiroStorage:string ='⠀⠀⠀⠀500 GB Storage⠀⠀⠀⠀' 
+  primeiroUser:string = '⠀⠀⠀⠀2 Users Allowed⠀⠀⠀⠀' 
+  primeiroSendUp:string = '⠀⠀⠀⠀Send up to 3 GB⠀⠀⠀⠀' 
+
+  segundoStorage:string = '⠀⠀⠀⠀⠀⠀1 TB Storage⠀⠀⠀⠀⠀⠀' 
+  segundoUser:string = '⠀⠀⠀⠀⠀5 Users Allowed⠀⠀⠀⠀⠀' 
+  segundoSendUp:string = '⠀⠀⠀⠀⠀Send up to 10 GB⠀⠀⠀⠀'
+
+  terceiroStorage:string = '⠀⠀⠀⠀⠀2 TB Storage⠀⠀⠀⠀⠀' 
+  terceiroUser:string = '⠀⠀⠀10 Users Allowed⠀⠀⠀⠀' 
+  terceiroSendUp:string = '⠀⠀⠀Send up to 20 GB⠀⠀⠀⠀' 
 }
